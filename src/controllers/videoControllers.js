42be380ce3aca275899cb4035db77bbe22318437
Ju-wang <charles09099@gmail.com>
2022-02-14 17:44:14 +0900

    node js

--- src/controllers/videoControllers.js
+++ src/controllers/videoControllers.js
@@ -1,6 +1,31 @@
-export const trending = (req, res) => res.send("home page videos")
-export const watch = (req, res) => res.send("watch")
-export const edit = (req, res) => res.send("edit")
-export const search = (req, res) => res.send("search")
-export const upload = (req, res) => res.send("upload")
-export const deleteVideo = (req, res) => res.send("delete video")
+import Video from "../models/Video"
+
+export const home = async (req, res) => {
+    const videos = await Video.find({})
+    return res.render("home", { pageTitle: "Home", videos })
+}
+export const watch = (req, res) => {
+    const id = req.params.id
+    const video = videos[id - 1]
+    return res.render("watch", { pageTitle: `Watching` })
+}
+export const getEdit = (req, res) => {
+    const id = req.params.id
+    const video = videos[id - 1]
+    return res.render("edit", { pageTitle: `Editing` })
+}
+export const postEdit = (req, res) => {
+    const id = req.params.id
+    const title = req.body.title
+    videos[id - 1].title = title
+    return res.redirect(`/video/${id}`)
+}
+
+export const getUpload = (req, res) => {
+    return res.render("upload", { pageTitle: "Upload Video" })
+}
+
+export const postUpload = (req, res) => {
+    const { title } = req.body
+    return res.redirect("/")
+}
