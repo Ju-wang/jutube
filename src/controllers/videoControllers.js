ee008a48d23dd06c447c65f3c40f8a6e73f87b92
Ju-wang <charles09099@gmail.com>
2022-02-24 16:45:20 +0900

    node js

--- src/controllers/videoControllers.js
+++ src/controllers/videoControllers.js
@@ -4,28 +4,55 @@ export const home = async (req, res) => {
     const videos = await Video.find({})
     return res.render("home", { pageTitle: "Home", videos })
 }
-export const watch = (req, res) => {
-    const id = req.params.id
-    const video = videos[id - 1]
-    return res.render("watch", { pageTitle: `Watching` })
+export const watch = async (req, res) => {
+    const { id } = req.params
+    const video = await Video.findById(id)
+    if (video) {
+        return res.render("watch", { pageTitle: video.title, video })
+    } else {
+        return res.render("404", { pageTitle: "Video not found" })
+    }
 }
-export const getEdit = (req, res) => {
+export const getEdit = async (req, res) => {
     const id = req.params.id
-    const video = videos[id - 1]
-    return res.render("edit", { pageTitle: `Editing` })
+    const video = await Video.findById(id)
+    if (!video) {
+        return res.render("404", { pageTitle: "Video not found" })
+    } else {
+        return res.render("edit", { pageTitle: `Editing: ${video.title}`, video })
+    }
 }
-export const postEdit = (req, res) => {
+export const postEdit = async (req, res) => {
     const id = req.params.id
-    const title = req.body.title
-    videos[id - 1].title = title
-    return res.redirect(`/video/${id}`)
+    const { title, description, hashtags } = req.body
+    const video = await Video.exists({ _id: id })
+    if (!video) {
+        return res.render("404", { pageTitle: "Video not found" })
+    } else {
+        await Video.findByIdAndUpdate(id, {
+            title,
+            description,
+            hashtags: Video.formatHashtags(hashtags)
+        })
+        return res.redirect(`/videos/${id}`)
+    }
 }
 
 export const getUpload = (req, res) => {
     return res.render("upload", { pageTitle: "Upload Video" })
 }
 
-export const postUpload = (req, res) => {
-    const { title } = req.body
-    return res.redirect("/")
+export const postUpload = async (req, res) => {
+    const { title, description, hashtags } = req.body
+    try {
+        await Video.create({
+            title,
+            description,
+            hashtags: Video.formatHashtags(hashtags)
+        })
+        return res.redirect("/")
+    } catch (error) {
+        console.log(error)
+        return res.render("upload", { pageTitle: "Upload Video", errorMessage: error._message })
+    }
 }
