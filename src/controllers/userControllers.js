bee8796942aa13aa59c5e006ddca0f64d9b59656
Ju-wang <charles09099@gmail.com>
2022-03-10 17:50:13 +0900

    node js

--- src/controllers/userControllers.js
+++ src/controllers/userControllers.js
@@ -1,9 +1,10 @@
 import User from "../models/User"
+import bcrypt from "bcrypt"
 
 export const getJoin = (req, res) => res.render("join", { pageTitle: "Join" })
 export const postJoin = async (req, res) => {
     const { name, username, email, password, password2, location } = req.body
-    const pageTitle = "pageTitle"
+    const pageTitle = "Join"
     if (password !== password2) {
         return res.status(400).render("join", {
             pageTitle,
@@ -38,13 +39,18 @@ export const remove = (req, res) => res.send("delete user")
 export const getLogin = (req, res) => res.render("login", { pageTitle: "Log In" })
 export const postLogin = async (req, res) => {
     const { username, password } = req.body
-    const exists = await User.exists({ username })
-    if (!exists) {
-        return res.status(400).render("login", { pageTitle: "Login", errorMessage: "This username dose not exists" })
+    const pageTitle = "Login"
+    const user = await User.findOne({ username })
+    if (!user) {
+        return res.status(400).render("login", { pageTitle, errorMessage: "This username dose not exists" })
     }
-    // check if account exists
-    // check if password correct
-    res.end()
+    const ok = await bcrypt.compare(password, user.password)
+    if (!ok) {
+        return res.status(400).render("login", { pageTitle, errorMessage: "Wrong password" })
+    }
+    req.session.loggedIn = true
+    req.session.user = user
+    return res.redirect("/")
 }
 export const logout = (req, res) => res.send("logout")
 export const see = (req, res) => res.send("see user")
